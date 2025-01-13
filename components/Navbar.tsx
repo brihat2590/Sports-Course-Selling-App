0bc3a7f5fbe9bafb686f80a1f68b35a6003da295
brihat2590 <thapabrihatbir321@gmail.com>
2025-01-13 09:39:18 +0530

    completed the navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -12,10 +12,29 @@ function Navbar({ className }: { className?: string }) {
       className={cn("fixed top-10 inset-x-0 max-w-2xl mx-auto z-50", className)}>
         
         <Menu setActive={setActive}>
-            <Link href='/'><MenuItem setActive={setActive} active={active} item="Home"></MenuItem>
+            ,<Link href='/'><MenuItem setActive={setActive} active={active} item="Home"></MenuItem></Link>
+            
+            <MenuItem setActive={setActive} active={active} item="Courses">
+            <div className="flex flex-col text-sm space-y-4">
+              <HoveredLink href="/courses">Football Fundamentals</HoveredLink>
+              <HoveredLink href="/courses">Basketball Masterclass</HoveredLink>
+              <HoveredLink href="/courses">Swim Sesh</HoveredLink>
+              <HoveredLink href="/courses">Badminton Practice</HoveredLink>
+              <HoveredLink href="/courses">Basic Sports Nutrition</HoveredLink>
+
+            </div>
+            
+            
+            
+            </MenuItem>
+            <Link href='/contact'>
+              <MenuItem setActive={setActive} active={active} item="Contact us">
+
+              </MenuItem>
             </Link>
-            <MenuItem setActive={setActive} active={active} item="Courses"></MenuItem>
-            <MenuItem setActive={setActive} active={active} item="Services"></MenuItem>
+            
+
+            
         </Menu>
 
     </div>
