7e073ff63e67aeca6ab966e75b841d8416f59faf
brihat2590 <thapabrihatbir321@gmail.com>
2025-01-21 15:26:37 +0530

    fixed and added images in courses section

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['images.unsplash.com','imgs.search.brave.com','www.instagram.com']
+    domains: ['images.unsplash.com','imgs.search.brave.com','www.instagram.com','www.example.com','plus.unsplash.com']
      // Add the domain here
   },
   /* config options here */
